4632fa68bf258010496d5c9dc1b0016d38b990af
Oluwafemi Akinruntan <114269718+Phemmy-Dev@users.noreply.github.com>
2022-10-14 22:14:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+// display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+// evaluates the expression and return result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
